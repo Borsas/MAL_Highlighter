fd560bb0e18c7f6af14b8714ffe27faa4f937c5e
Borsas <borsasyt@gmail.com>
2019-12-27 17:55:22 +0200

    Refactored back-end to support manga also, currently doesnt care if anime and manga have same ID so it will highlight incorrectly sometiems

--- mal.js
+++ mal.js
@@ -11,156 +11,157 @@
 (function() {
   'use strict';
 
+  class Highlighter {
+    constructor(type, username){
+      this.type = type;
+      this.username = username;
+    }
+
+
   // Get data from MALs json handler or from sessionstorage, depending which has it.
-  async function getData(username){
-
-    if(sessionStorage.getItem("animeData")){
-      console.log("Loaded data from memory");
-      return JSON.parse(sessionStorage.getItem("animeData"));
-
-    } else {  
-    let response =  await fetch('https://myanimelist.net/animelist/'+ username +'/load.json?status=7');
-    let data = await response.json();
-
-    sessionStorage.setItem("animeData", JSON.stringify(data));
-    console.log("Loaded data from MAL API");
+    async getData() {
+    if (sessionStorage.getItem(this.type)){
+      console.log('Loaded ' + this.type + ' from memory');
+      return JSON.parse(sessionStorage.getItem(this.type));
+    } else {
+    let data = await (await fetch('https://myanimelist.net/' + this.type +'list/'+ this.username +'/load.json?status=7')).json();
+    sessionStorage.setItem(this.type, JSON.stringify(data));
+    console.log('Loaded ' + this.type +' from MAL API');
     return data;
     }
   }
 
 
   // Adds attributes to the correct elements
-  function addAttributes(statusTypes, id, element){
-   if (statusTypes["watching"].includes(parseInt(id))){
-      element.classList.add("HL-watching");
-
-    }else if (statusTypes["completed"].includes(parseInt(id))){
-      element.classList.add("HL-completed");
-
-    }else if (statusTypes["onHold"].includes(parseInt(id))){
-      element.classList.add("HL-onHold");
-
-    }else if (statusTypes["dropped"].includes(parseInt(id))){
-      element.classList.add("HL-dropped");
-
-    }else if (statusTypes["planToWatch"].includes(parseInt(id))){
-      element.classList.add("HL-planToWatch");
+  addAttributes(id, element){
+   if (this.statusType['watching'].includes(parseInt(id))){
+      element.classList.add('HL-watching');
+    } else if (this.statusType['completed'].includes(parseInt(id))){
+      element.classList.add('HL-completed');
+    } else if (this.statusType['onHold'].includes(parseInt(id))){
+      element.classList.add('HL-onHold');
+    } else if (this.statusType['dropped'].includes(parseInt(id))){
+      element.classList.add('HL-dropped');
+    } else if (this.statusType['planToWatch'].includes(parseInt(id))){
+      element.classList.add('HL-planToWatch');
     }
   }
 
   // Change color on myanimelist.net/topanime.php
-  function colorTopAnime(statusTypes){
-    var tr = document.getElementsByClassName("ranking-list");
+  colorTopAnime(){
+    let tr = document.getElementsByClassName('ranking-list');
 
-    for (var i = 0; i < tr.length; i++){
-      var id = tr[i].getElementsByTagName("a")[0].getAttribute("href").split("/")[4];
-      addAttributes(statusTypes, id, tr[i]);
+    for (let i = 0; i < tr.length; i++){
+      let id = tr[i].getElementsByTagName('a')[0].getAttribute('href').split('/')[4];
+      this.addAttributes(id, tr[i]);
     }
   }
 
   // Change color on myanimelist.net/people/*/*
-  function colorPeoplePage(statusTypes){
-    var tr = document.getElementsByTagName("tr");
+  colorPeoplePage(){
+    let tr = document.getElementsByTagName('tr');
 
     // Count of garbage tr classes in the bottom that are not wanted
     // people-comment classes are also added to this if there are any
     // and manga are also added to this because it will color them with anime colors
-    var bottom = 6;
+    let bottom = 6;
 
-    if(document.getElementsByClassName("people-comment")){
-       bottom += document.getElementsByClassName("people-comment").length;
+    if (document.getElementsByClassName('people-comment')){
+       bottom += document.getElementsByClassName('people-comment').length;
        }
 
-     if(document.getElementsByTagName("tbody")[4].getElementsByTagName("tr")){
-       bottom += document.getElementsByTagName("tbody")[4].getElementsByTagName("tr").length;
+     if (document.getElementsByTagName('tbody')[4].getElementsByTagName('tr')){
+       bottom += document.getElementsByTagName('tbody')[4].getElementsByTagName('tr').length;
        }
 
-    for (var i = 4; i < tr.length - bottom; i++){
-     var series = tr[i].getElementsByTagName("a")[1];
-     var url = series.getAttribute("href").split("/")
+    for (let i = 4; i < tr.length - bottom; i++){
+     let series = tr[i].getElementsByTagName('a')[1];
+     let url = series.getAttribute('href').split('/');
 
-     if (url.length == 6){var id = url[4]}
-      addAttributes(statusTypes, id, tr[i]);
+     if (url.length == 6){
+        let id = url[4];
+        this.addAttributes(id, tr[i]);
+      }
     }
   }
 
   // Change color on producer and season page.
-  function colorGenericPage(statusTypes){
-    var allShows = document.getElementsByClassName("seasonal-anime");
+  colorGenericPage(){
+    let allShows = document.getElementsByClassName('seasonal-anime');
 
-    for (var i = 0; i < allShows.length; i++){
-      var id = allShows[i].getElementsByClassName("link-title")[0].getAttribute("href").split("/")[4];
-      addAttributes(statusTypes, id, allShows[i]);
+    for (let i = 0; i < allShows.length; i++){
+      let id = allShows[i].getElementsByClassName('link-title')[0].getAttribute('href').split('/')[4];
+      this.addAttributes(id, allShows[i]);
     }
   }
 
-  //Sorts all statuses from the json
-  function getStatusTypes(data){
-    var statusTypes = {watching: [], completed: [], onHold:[], dropped:[], planToWatch: []};
+  // Sorts all statuses from the json
+  getStatusType(data){
+    this.statusType = {watching: [], completed: [], onHold: [], dropped: [], planToWatch: []};
+    let id = this.type + '_id';
 
-    for(var property in data){
+    for (let property in data){
       if (data[property].status === 1){
-        statusTypes["watching"].push(data[property].anime_id);
-
-      }else if (data[property].status === 2){
-        statusTypes["completed"].push(data[property].anime_id);
-
-      }else if (data[property].status === 3){
-        statusTypes["onHold"].push(data[property].anime_id);
-      }
-      else if (data[property].status === 4){
-        statusTypes["dropped"].push(data[property].anime_id);
-      }
-      else if (data[property].status === 6){
-        statusTypes["planToWatch"].push(data[property].anime_id);
+        this.statusType['watching'].push(data[property][id]);
+      } else if (data[property].status === 2){
+        this.statusType['completed'].push(data[property][id]);
+      } else if (data[property].status === 3){
+        this.statusType['onHold'].push(data[property][id]);
+      } else if (data[property].status === 4){
+        this.statusType['dropped'].push(data[property][id]);
+      } else if (data[property].status === 6){
+        this.statusType['planToWatch'].push(data[property][id]);
       }
     }
-    return statusTypes
   }
 
-  // Get username
-  var user = document.getElementsByClassName('header-profile-link')[0].text;
-
-  //if(document.getElementsByClassName("btn-user-status-add-list")){
-    //document.getElementsByClassName("btn-user-status-add-list")[0].setAttribute('onclick', 'sessionStorage.clear(); return false;');
-    //console.log("clear button");
-  //}
 
   // Inject CSS
-  $('<style type="text/css" />').html(
-    `.information, .your-score .text {
-        color: #323232 !important;}
-    .top-anime-rank-text.rank1, .top-anime-rank-text.rank2 {
-        color: #5d5c5c;}
-    .top-ranking-table tr.ranking-list td {
-      background-color: transparent !important;}
-    .HL-watching {
-        background-color: #34ce0f !important;}
-    .HL-completed {
-        background-color: #ccede4 !important;}
-    .HL-onHold {
-        background-color: #f1c83e !important;}
-    .HL-dropped {
-        background-color: #f76265 !important;}
-    .HL-planToWatch {
-        background-color: #dcc8aa !important;}`
+  injectCs(){
+    $('<style type="text/css"/>').html(
+      `.information, .your-score .text {
+          color: #323232 !important;}
+      .top-anime-rank-text.rank1, .top-anime-rank-text.rank2 {
+          color: #5d5c5c;}
+      .top-ranking-table tr.ranking-list td {
+        background-color: transparent !important;}
+      .HL-watching {
+          background-color: #34ce0f !important;}
+      .HL-completed {
+          background-color: #ccede4 !important;}
+      .HL-onHold {
+          background-color: #f1c83e !important;}
+      .HL-dropped {
+          background-color: #f76265 !important;}
+      .HL-planToWatch {
+          background-color: #dcc8aa !important;}`
 
-    ).appendTo('head');
-
-  // Main function, this shit runs it all
-  async function main(){
-    var data = await getData(user);
-    var statusTypes = getStatusTypes(data);
-    var url = window.location.href;
-
-    if (url.match(/^https?:\/\/myanimelist\.net\/topanime\.php/)){
-      colorTopAnime(statusTypes);
-    } else if(url.match(/^https?:\/\/myanimelist\.net\/people\/\d*\/.*/)){
-      colorPeoplePage(statusTypes);
-    }else if (url.match(/^https?:\/\/myanimelist\.net\/anime\/(season((\d*\/.*|$)|^\s*$)|producer\/*)/)){
-      colorGenericPage(statusTypes);
-    }
+      ).appendTo('head');
   }
 
-  main();
-  })();
+  // Main function, this shit runs it all
+  async main() {
+    let data = await this.getData();
+    this.injectCs();
+
+    this.getStatusType(data);
+
+
+    let url = window.location.href;
+
+    if (url.match(/^https?:\/\/myanimelist\.net\/(topmanga|topanime)\.php/)){
+      this.colorTopAnime();
+    } else if (url.match(/^https?:\/\/myanimelist\.net\/people\/\d*\/.*/)){
+      this.colorPeoplePage();
+    } else if (url.match(/^https?:\/\/myanimelist\.net\/anime\/(season((\d*\/.*|$)|^\s*$)|producer\/*)/)){
+      this.colorGenericPage();
+    }
+  }
+}
+  let user = document.getElementsByClassName('header-profile-link')[0].text;
+
+  const animeHL = new Highlighter('anime', user);
+  animeHL.main();
+  const mangaHL = new Highlighter('manga', user);
+  mangaHL.main();
+ })();
