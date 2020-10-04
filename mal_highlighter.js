19266cfe651e79ec54b5ef865740e34fbe1cc409
Borsas <borsasyt@gmail.com>
2020-10-04 20:27:15 +0300

    Version increase, changed file name

--- mal_highlighter.js
+++ mal_highlighter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MAL Highlighter
 // @namespace    http://keittokilta.fi
-// @version      2.2.2
+// @version      2.2.3
 // @description  Highlights MAL titles with different colors.
 // @author       Borsas
 // @match        https://myanimelist.net/*
